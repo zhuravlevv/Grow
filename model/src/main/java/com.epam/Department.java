ce1d4ed070a27c7718b314df0a2f49b66b0e7dd3
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-06-18 22:28:02 +0300

    add Department model

---
+++ model/src/main/java/com.epam/Department.java
@@ -0,0 +1,31 @@
+package com.epam;
+
+public class Department {
+
+    private Integer id;
+
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
