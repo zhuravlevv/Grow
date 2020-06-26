bdf19e8a08ecc2b865ae2b3668dcd196aa1f5dce
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-06-26 03:38:51 +0300

    add DepartmentDtoService test

---
+++ service/src/test/java/com/epam/DepartmentDtoServiceTest.java
@@ -0,0 +1,37 @@
+package com.epam;
+
+import com.epam.dto.DepartmentDto;
+import com.epam.impl.DepartmentDtoServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.Assert;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(locations = {"classpath*:test-service.xml"})
+public class DepartmentDtoServiceTest {
+
+    @InjectMocks
+    private DepartmentDtoServiceImpl departmentDtoService;
+
+    @Mock
+    private DepartmentDtoDao departmentDtoDao;
+
+    @Test
+    public void getAll(){
+        Mockito.when(departmentDtoDao.getAllWithAvgSalary()).thenReturn(new LinkedList<>());
+
+        List<DepartmentDto> departments = departmentDtoService.getAllWithAvgSalary();
+        Assert.notNull(departments);
+    }
+
+}
