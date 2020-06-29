7c408e9a64fc3ef93c5c61023e322249e7ce77d4
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-06-29 03:39:33 +0300

    add DepartemntController IT

--- webapp/src/main/java/com/epam/web_app/DepartmentController.java
+++ webapp/src/main/java/com/epam/web_app/DepartmentController.java
@@ -1,5 +1,9 @@
 package com.epam.web_app;
 
+import com.epam.service.DepartmentDtoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +11,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DepartmentController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
+    private final DepartmentDtoService departmentDtoService;
+
+    public DepartmentController(DepartmentDtoService departmentDtoService) {
+        this.departmentDtoService = departmentDtoService;
+    }
+
     @GetMapping(value = "/departments")
     public final String departments(Model model) {
+        LOGGER.debug("departments()");
+        model.addAttribute("departments", departmentDtoService.getAllWithAvgSalary());
         return "departments";
     }
 
---
+++ webapp/src/test/java/com/epam/web_app/DepartmentControllerIT.java
@@ -0,0 +1,68 @@
+package com.epam.web_app;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.hamcrest.Matchers.*;
+
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:app-context-test.xml"})
+public class DepartmentControllerIT {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void shouldReturnDepartmentsPage() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/departments")
+        ).andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("departments"))
+                .andExpect(model().attribute("departments", hasItem(
+                        allOf(
+                                hasProperty("id", is(1)),
+                                hasProperty("name", is("SEO")),
+                                hasProperty("averageSalary", is(500.00))
+                        )
+                )))
+                .andExpect(model().attribute("departments", hasItem(
+                        allOf(
+                                hasProperty("id", is(2)),
+                                hasProperty("name", is("Development")),
+                                hasProperty("averageSalary", is(1900.00))
+                        )
+                )))
+                .andExpect(model().attribute("departments", hasItem(
+                        allOf(
+                                hasProperty("id", is(3)),
+                                hasProperty("name", is("Production")),
+                                hasProperty("averageSalary", is(0.00))
+                        )
+                )));
+    }
+}
