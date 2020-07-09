eb583c6ee0af7f6fc740c064db0bc99ff3763349
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-07-09 19:39:24 +0300

    add DepartmentDtoController

--- rest/src/main/java/com/epam/rest/DepartmentDtoController.java
+++ rest/src/main/java/com/epam/rest/DepartmentDtoController.java
@@ -1,4 +1,28 @@
 package com.epam.rest;
 
+import com.epam.model.dto.DepartmentDto;
+import com.epam.service.DepartmentDtoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class DepartmentDtoController {
+
+    private final DepartmentDtoService departmentDtoService;
+
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentDtoController.class);
+
+    public DepartmentDtoController(DepartmentDtoService departmentDtoService) {
+        this.departmentDtoService = departmentDtoService;
+    }
+
+    @GetMapping("/department/avg")
+    public List<DepartmentDto> getAll(){
+        LOGGER.trace("getAll()");
+        return departmentDtoService.getAllWithAvgSalary();
+    }
 }
